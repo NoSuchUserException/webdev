c9ed7ce92dea7b1e2c4fb4ab4062edb877145c6b
mackaveli07 <60677021+mackaveli07@users.noreply.github.com>
2020-04-28 16:45:53 -0500

    Update FormtoEmail.php
    added the email address for the form to be sent to and added the subject line

--- FormtoEmail.php
+++ FormtoEmail.php
@@ -2,8 +2,8 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "you@yourdomain.com";
-    $email_subject = "Your email subject line";
+    $email_to = "admin@etarium.us";
+    $email_subject = "Advice for Work-Life Balance";
  
     function died($error) {
         // your error code can go here
